d52bdf80049a39c93186f8e6641c61ad06ef6351
Hayley <git@codingcat.nl>
2026-02-17 20:16:24 +0100

    🐛 fix rate-limiter/auth ordering bug + restructure

--- server/src/user_routes.rs
+++ server/src/user_routes.rs
@@ -2,6 +2,8 @@ use axum::{Router, middleware::from_fn_with_state};
 
 use crate::auth_cache::redirect_if_unauthorized;
 
+/// User page router with auth redirect middleware.
+/// Unauthenticated requests get bounced to login.
 pub fn router(appstate: crate::Interfaces, user_folder: &str) -> Router<crate::Interfaces> {
     let serve_dir = tower_http::services::ServeDir::new(user_folder);
     Router::new()
